f8dc696e669a9274d563ffdb6a724244e53c4981
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2026-09-10 14:19:36 -0300

    add(config): nvim latex compilation buffer

--- .config/nvim/lua/config/tex_cmds.lua
+++ .config/nvim/lua/config/tex_cmds.lua
@@ -21,3 +21,36 @@ vim.api.nvim_create_user_command("LatexFixQuotes", function()
     %s/"\([^"]*\)"/``\1''/g   
   ]])
 end, {})
+
+vim.api.nvim_create_user_command("OpenPdf", function(opts)
+  local file = opts.args ~= "" and opts.args or "main.pdf"
+
+  vim.fn.jobstart({ "xdg-open", file }, {
+    detach = true,
+  })
+end, {
+  nargs = "?",
+  complete = "file",
+})
+
+vim.api.nvim_create_user_command("LatexCompile", function()
+  local file = vim.fn.expand("%:p")
+
+  if file == "" then
+    vim.notify("No hay un archivo abierto", vim.log.levels.ERROR)
+    return
+  end
+
+  local dir = vim.fn.fnamemodify(file, ":h")
+  local name = vim.fn.fnamemodify(file, ":t")
+
+  vim.cmd("botright 15split")
+  vim.cmd("enew")
+
+  vim.fn.jobstart({ "lualatex", name }, {
+    term = true,
+    cwd = dir,
+  })
+end, {
+  desc = "Compilar el archivo actual con LuaLaTeX",
+})
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,4 +3,8 @@ vim.keymap.set("n", "<leader>ls", function()
   require("config.latex_symbols").show()
 end, { desc = "Insertar símbolo LaTeX" })
 
+vim.keymap.set("n", "<leader>lc", "<cmd>LatexCompile<CR>", {
+  desc = "Compilar LaTeX",
+})
+
 require("config.tex_cmds")
